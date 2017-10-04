f29521b774f049fb7581a2529582dc5a3adff2b0
Continuities <michael@doublespeakgames.com>
2017-10-03 22:14:04 -0400

    Fresh coat of paint

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className={styles.App}>
         <header className={styles.header}>
           <img src={logo} className={styles.logo} alt="logo" />
-          <h1 className={styles.title}>Welcome to React</h1>
+          <h1 className={styles.title}>ENLIST NOW</h1>
         </header>
         <p className={styles.intro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Service guarantees citizenship
         </p>
       </div>
     );
