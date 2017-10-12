71d5c4ffde5191e3b8af3b45cdead70161b5ab79
Continuities <michael@doublespeakgames.com>
2017-10-12 19:57:33 -0400

    Playing with Travis CI

--- scripts/test-all.js
+++ scripts/test-all.js
@@ -4,4 +4,4 @@ const opts = { stdio: 'inherit', cwd: '.', shell: true };
 const child = require('child_process');
 
 child.spawn('yarn', [ 'client:test' ], opts);
-child.spawn('yarn', [ 'server:test' ], opts);
+//child.spawn('yarn', [ 'server:test' ], opts);
