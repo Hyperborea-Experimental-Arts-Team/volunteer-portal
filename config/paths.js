25231a8999a0ae6319ffb135641f8fe39028eefd
Continuities <michael@doublespeakgames.com>
2017-10-12 21:58:13 -0400

    Writing deploy scripts for digital ocean

--- config/paths.js
+++ config/paths.js
@@ -42,6 +42,7 @@ function getServedPath(appPackageJson) {
 module.exports = {
   dotenv: resolveApp('.env'),
   appBuild: resolveApp('build/client'),
+  appDeploy: resolveApp('deploy'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveApp('src/client/index.js'),
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,12 @@
 'use strict';
 
+const rmrf = require('rimraf');
+const fs = require('fs');
+const paths = require('../config/paths');
+
 console.log('Deploying...');
-// TODO
+
+rmrf.sync(paths.appDeploy);
+fs.mkdirSync(paths.appDeploy);
+
+// Copy the stuff we need to
