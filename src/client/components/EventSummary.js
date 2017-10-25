ab946641ec62ab91a224c46a1349a289f70d55be
Continuities <michael@doublespeakgames.com>
2017-10-24 23:21:32 -0400

    Skinned image and header sections of event summary card.

--- src/client/components/EventSummary.js
+++ src/client/components/EventSummary.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
+import { concat } from '../util';
+
+import theme from '../theme.css';
 import style from './EventSummary.css';
 
 export default ({ event }) => (
   <div className={style.EventSummary}>
-    <div className={style.image}></div>
-    <div className={style.header}>
-      <h3 className={style.name}>{event.name}</h3>
-      <span class={style.num}>{event.numDepartments}</span>
-      <FormattedMessage
-          id="event.departments"
-          defaultMessage="{n, plural, one {Department} other {Departments}}"
-          values={{n: event.numDepartments}}
-      />
-      <span class={style.num}>{event.numVolunteers}</span>
-      <FormattedMessage
-          id="event.volunteers"
-          defaultMessage="{n, plural, one {Volunteer} other {Volunteers}}"
-          values={{n: event.volunteers}}
-      />
+    <div className={style.image}>
+      <div className={concat(style.header, theme.txt_1)}>
+        <h3 className={style.name}>{event.name}</h3>
+        {event.numDepartments}&nbsp;
+        <span className={theme.txt_3}>
+          <FormattedMessage
+              id="event.departments"
+              defaultMessage="{n, plural, one {Department} other {Departments}}"
+              values={{n: event.numDepartments}}
+          />
+        ,&#32;</span>
+        {event.numVolunteers}&nbsp;
+        <span className={theme.txt_3}>
+          <FormattedMessage
+              id="event.volunteers"
+              defaultMessage="{n, plural, one {Volunteer} other {Volunteers}}"
+              values={{n: event.volunteers}}
+          />
+        </span>
+      </div>
     </div>
-
-
   </div>
 );
