d6ae0eff48bd9f9cc2498c8fd47e1e0ed2806b67
Continuities <michael@doublespeakgames.com>
2017-10-29 16:24:51 -0400

    Added link back to events list

--- src/client/components/Brand.js
+++ src/client/components/Brand.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import style from './Brand.less';
 
 export default ({ size }) => (
-  <div className={style.brand} style={{fontSize: `${size}px`}}>Nyan</div>
+  <Link to="/events" className={style.brand} style={{fontSize: `${size}px`}}>Nyan</Link>
 );
