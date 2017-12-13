82c8091d37e306562912d6b7cc0a1be3e5e6471a
Continuities <michael@doublespeakgames.com>
2017-12-12 21:12:35 -0500

    Added search submenu

--- src/client/components/EventDepartments.js
+++ src/client/components/EventDepartments.js
@@ -5,10 +5,16 @@ import { concat } from '../util';
 import DataLoader from '../containers/DataLoader';
 import Image from './Image';
 import UserBadge from './UserBadge';
+import SubMenu from './SubMenu';
+import FormField from './FormField';
+import Button from './Button';
 
 import grid from '../grid.less';
 import theme from '../theme.css';
 import style from './EventDepartments.less';
+import searchSvg from '../images/search.svg';
+import addSvg from '../images/add.svg';
+import editSvg from '../images/edit.svg';
 
 const LeadInfo = lead => (
   <div className={theme.txt_darkest}>
@@ -28,7 +34,7 @@ const LeadInfo = lead => (
 );
 
 const DepartmentNumbers = () => (
-  <div className={concat(style.numberSection, theme.divider)}>
+  <div className={concat(style.numberSection, theme.divider)} style={{ minHeight: '200px' }}>
     TODO
   </div>
 );
@@ -59,24 +65,76 @@ const DepartmentList = ({ departments }) => (
 );
 
 const SummaryRow = ({ eventId }) => (
-  <div className={grid.row}>
-    <section className={grid.col_sm_4} style={{position: 'relative'}}>
-      <PageTitle className={theme.txt_lightest}
-                 title={<FormattedMessage id="event.teams" defaultMessage="Departments & Teams" />}
-                 link={`/event/${eventId}/overview`} />
-      <DataLoader serviceCall={`events/${eventId}/lead`} component={LeadInfo} />
-    </section>
-    <section className={grid.col_sm_8}>
-      <div className={concat(style.info, theme.bg_content)}>
-        <DepartmentNumbers />
-        <DataLoader serviceCall={`events/${eventId}/departments`} component={DepartmentList} />
-      </div>
-    </section>
+  <div className={theme.page_padding}>
+    <div className={grid.row}>
+      <section className={grid.col_sm_4} style={{position: 'relative'}}>
+        <PageTitle className={theme.txt_lightest}
+                   title={<FormattedMessage id="event.teams" defaultMessage="Departments & Teams" />}
+                   link={`/event/${eventId}/overview`} />
+        <DataLoader serviceCall={`events/${eventId}/lead`} component={LeadInfo} />
+        <Button text={<FormattedMessage
+                          id="department.create"
+                          defaultMessage="Create New Department"/>}
+                icon={addSvg}
+                className={concat(style.button, theme.bg_3, theme.txt_lightest)}
+        />
+        <Button text={<FormattedMessage
+                          id="department.edit"
+                          defaultMessage="Edit Departments"/>}
+                icon={editSvg}
+                className={concat(style.button, theme.bg_2, theme.txt_lightest)}
+        />
+      </section>
+      <section className={grid.col_sm_8}>
+        <div className={concat(style.info, theme.bg_content)}>
+          <DepartmentNumbers />
+          <DataLoader serviceCall={`events/${eventId}/departments`} component={DepartmentList} />
+        </div>
+      </section>
+    </div>
   </div>
 );
 
+const Departments = ({ }) => (
+  <div className={theme.page_padding}>
+    <div className={theme.bg_content} style={{ height: '100px' }}>
+    </div>
+  </div>
+);
+
+class SearchForm extends React.Component {
+  constructor() {
+    super();
+    this.state = { search: '' };
+  }
+
+  updateSearch(value) {
+    this.setState(Object.assign({}, this.state, {
+      search: value
+    }));
+  }
+
+  render() {
+    return (
+        <form className={concat(style.search, theme.txt_lightest)}>
+          <FormField name="search"
+                     icon={searchSvg}
+                     value={this.state.search}
+                     onChange={v => this.updateSearch(v)}
+                     title={<FormattedMessage
+                               id="search"
+                               defaultMessage="Search" />} />
+        </form>
+    );
+  }
+}
+
 export default ({ eventId }) => (
-  <div className={concat(style.wrap, theme.page_padding)}>
+  <div>
     <SummaryRow eventId={eventId} />
+    <SubMenu>
+      <SearchForm />
+    </SubMenu>
+    <Departments />
   </div>
 );
